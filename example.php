a8ab35c13d26a5419d8e5b27f6f26e2618cfc11d
Aldous Scales <aldous@dalegroup.net>
2017-04-23 18:34:42 +1000

    commit

---
+++ example.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once('twitter.class.php');
+
+// Show errors for dev
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+$twitter = new Twitter();
+
+
+for ($i = 0; $i < 900; $i++) {
+	$twitter->collect_tweets();	
+}
+
+// Collect and store tweets in db
+
+// $twitter->collect_tweets();	
+
+
+// $tweets = $twitter->get_tweets();
+// print_r($tweets);
+
+// Get remaining requests for 15 min window
+// $rates = $twitter->get_rate_limit();
+// print_r($rates);
+
+?>
--- twitter.class.php
+++ twitter.class.php
@@ -21,33 +21,37 @@ class Twitter {
 
 	}
 
-	/*
-	Get all third party accounts
+	/**
+	* Get all third party accounts
 	*/
 	public function get_third_party_accounts() {
 		// Get accounts from db or just hardcode them here
 		$accounts = ['Sydney_Uni'];
 		return $accounts;
-
 	}
 
-	public function check_last_lookup($user_ids = NULL) {
-		return true;
-		}
+	public function get_rate_limit() {
+		$connection = new TwitterOAuth($this->CONSUMER_KEY, $this->CONSUMER_SECRET, $this->access_token, $this->access_token_secret);
+			$response = $connection->get("application/rate_limit_status", ['resources' => 'statuses']);
+			if ($connection->getLastHttpCode() == 200) {
+				return $response->resources->statuses->{'/statuses/user_timeline'};
+			} else {
+				// Error
+			}
+	}
 
 
-	/*
-	Get the the id of the most recent tweet created by a user
-	Use the return to tell the lookup where to stop
+	/**
+	* Get the the id of the most recent tweet created by a user
+	* Use the return to tell the lookup where to stop
 	*/
 	public function get_last_tweet_id($user_id) {
 
 			try {
-
 				$dbh = new PDO("mysql:host=127.0.0.1;dbname=twitter;charset=utf8", "root", "root");
 				$dbh->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );  
 
-				$sql = "SELECT * FROM `tweets` WHERE `screen_name` = :screen_name ORDER BY `id` ASC LIMIT 1";
+				$sql = "SELECT * FROM `tweets` WHERE `screen_name` = :screen_name ORDER BY `id` DESC LIMIT 1";
 				$sth = $dbh->prepare($sql);
 				$sth->bindParam(":screen_name", $user_id);
 				$sth->execute();
@@ -60,12 +64,35 @@ class Twitter {
 
 	}
 
-	public function get_tweets($user_ids = NULL) {
+	/**
+	* Get tweets from db
+	*/
+	public function get_tweets() {
 
-		// Get all account ids ifs not supplied
+			try {
+				$dbh = new PDO("mysql:host=127.0.0.1;dbname=twitter;charset=utf8", "root", "root");
+				$dbh->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );  
+
+				$sql = "SELECT * FROM `tweets`";
+				$sth = $dbh->prepare($sql);
+				$sth->execute();
+				$results = $sth->fetchAll();
+				return $results;
+
+			} catch (PDOException $e) {
+				print_r($e);
+			}
+	}
+
+
+	/**
+	* Request new tweets from supplied users and store the result in db
+	*/
+	public function collect_tweets($user_ids = NULL) {
+
+		// Get all account ids if not supplied
 		if (!isset($user_ids)) {
 			$user_ids = $this->get_third_party_accounts();
-			// print_r($user_ids);
 		}
 
 		// does the connection need to be estalished every time?
@@ -75,19 +102,16 @@ class Twitter {
 		foreach ($user_ids as $user_id) {
 
 			$request_params = array(
-				'count' 			=> 100,
+				'count' 			=> 100, //max 3200
 				'exclude_replies' 	=> true,
-				'user_id' 			=> $user_id,
+				'screen_name' 		=> $user_id,
 				);
-
 			$since_id = $this->get_last_tweet_id($user_id);
-			
 			if (isset($since_id) && !empty($since_id)) {
 				$request_params['since_id'] = $since_id;		
 			}
 
 			$response = $connection->get("statuses/user_timeline", $request_params);
-
 			if ($connection->getLastHttpCode() != 200) {
 				// Error
 				print_r($response);
@@ -100,7 +124,7 @@ class Twitter {
 			Sample code - Insert tweet into db if it doesn't exist already
 			*/
 
-			print_r($response);
+			// print_r($response);
 			try {
 
 				$dbh = new PDO("mysql:host=127.0.0.1;dbname=twitter;charset=utf8", "root", "root");
@@ -109,17 +133,16 @@ class Twitter {
 				$sql = "INSERT IGNORE INTO `tweets`
 						(`id`, `screen_name`, `name`, `text`, `date_posted`, `lookup_date`)
 						VALUES (:id, :screen_name, :name, :tweet_text, :date_posted, CURRENT_DATE)";
+				$sth = $dbh->prepare($sql);
+
+				print_r($response);
 
 				foreach($response as $status) {
-
-					$sth = $dbh->prepare($sql);
- 					echo $status->id;
-					$sth->bindParam(":id", $status->id);
-					$sth->bindParam(":screen_name", $status->user->screen_name);
-					$sth->bindParam(":name", $status->user->name);
-					$sth->bindParam(":tweet_text", $status->text);
+					$sth->bindParam(":id", $status->id_str, PDO::PARAM_INT);
+					$sth->bindParam(":screen_name", $status->user->screen_name, PDO::PARAM_STR);
+					$sth->bindParam(":name", $status->user->name, PDO::PARAM_STR);
+					$sth->bindParam(":tweet_text", $status->text, PDO::PARAM_STR);
 					$sth->bindParam(":date_posted", $status->created_at);
-
 					$sth->execute();
 				}
 
@@ -127,27 +150,11 @@ class Twitter {
 				print_r($e);
 			}
 
-
 		}
-
 
 	}
 
 
 }
-
-
-// Show errors for dev
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-
-$twitter = new Twitter();
-$twitter->get_tweets();
-// $twitter->connect_test();
-
-
-
 
 ?>
